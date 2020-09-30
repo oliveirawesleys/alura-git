742b6adbf68a0e2d1506bbce9141ab768c93c380
Wesley <oliveira.wesleys@gmail.com>
2020-09-29 21:29:30 -0300

    Utilizando biblioteca Gson.

--- ecommerce/src/main/java/KafkaDispatcher.java
+++ ecommerce/src/main/java/KafkaDispatcher.java
@@ -5,27 +5,26 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-class KafkaDispatcher implements Closeable {
+class KafkaDispatcher<T>implements Closeable {
 
-    private final KafkaProducer<String, String> producer;
+    private final KafkaProducer<String, T> producer;
 
     KafkaDispatcher() {
-        this.producer = new KafkaProducer<String, String>(properties());
+        this.producer = new KafkaProducer<String, T>(properties());
     }
 
     private static Properties properties() {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
     }
 
-    void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+    void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
         var record = new ProducerRecord<>(topic, key, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
@@ -34,6 +33,7 @@ class KafkaDispatcher implements Closeable {
             }
             System.out.println("Success sending " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
         };
+        producer.send(record, callback).get();
     }
 
     @Override
--- ecommerce/src/main/java/NewOrderMain.java
+++ ecommerce/src/main/java/NewOrderMain.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.math.BigDecimal;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -11,17 +12,21 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
 
-            for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = "1212, 545433, 321229";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
 
-                var email = "Thank you! We are processing your order.";
-                var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, value);
+                    var email = "Thank you! We are processing your order.";
+                    //var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
