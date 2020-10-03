51606ec307eeff2f0100649ca7c67200f44c9ca5
Wesley <oliveira.wesleys@gmail.com>
2020-10-03 02:08:35 -0300

    Lista de vip de destinos

---
+++ listavip/src/main/java/br/com/alura/listavip/repository/DestinoRepository.java
@@ -0,0 +1,7 @@
+package br.com.alura.listavip.repository;
+
+import br.com.alura.listavip.model.Destino;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DestinoRepository extends CrudRepository<Destino, Long> {
+}
