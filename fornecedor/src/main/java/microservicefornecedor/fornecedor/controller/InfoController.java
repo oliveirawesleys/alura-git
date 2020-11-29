8e5c5735c5bc7209d9725280d18f0e2fd75f47f7
Wesley <oliveira.wesleys@gmail.com>
2020-11-28 23:03:19 -0300

    Implementando logback e sleuth

--- fornecedor/src/main/java/microservicefornecedor/fornecedor/controller/InfoController.java
+++ fornecedor/src/main/java/microservicefornecedor/fornecedor/controller/InfoController.java
@@ -2,6 +2,8 @@ package microservicefornecedor.fornecedor.controller;
 
 import microservicefornecedor.fornecedor.model.InfoFornecedor;
 import microservicefornecedor.fornecedor.service.InfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/info")
 public class InfoController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+
     @Autowired
     private InfoService infoService;
 
     @RequestMapping("/{estado}")
     public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
-        System.out.println(infoService.getInforPorEstado(estado));
+        //System.out.println(infoService.getInforPorEstado(estado));
+        LOG.info("Recebido pedido de informações do fornecedor de {}", estado);
         return infoService.getInforPorEstado(estado);
     }
 
--- fornecedor/src/main/java/microservicefornecedor/fornecedor/controller/PedidoController.java
+++ fornecedor/src/main/java/microservicefornecedor/fornecedor/controller/PedidoController.java
@@ -3,6 +3,8 @@ package microservicefornecedor.fornecedor.controller;
 import microservicefornecedor.fornecedor.dto.ItemDoPedidoDTO;
 import microservicefornecedor.fornecedor.model.Pedido;
 import microservicefornecedor.fornecedor.service.PedidoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +14,14 @@ import java.util.List;
 @RequestMapping("/pedido")
 public class PedidoController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
+
 	@Autowired
 	private PedidoService pedidoService;
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
+		LOG.info("Pedido recebido.");
 		return pedidoService.realizaPedido(produtos);
 	}
 	
