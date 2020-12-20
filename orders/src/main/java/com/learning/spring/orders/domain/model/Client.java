d445c1c5e8e394cd25c886e31f153bcd1754c06a
Wesley <oliveira.wesleys@gmail.com>
2020-12-20 16:52:01 -0300

    Projeto de estudo para ordem de serviço

---
+++ orders/src/main/java/com/learning/spring/orders/domain/model/Client.java
@@ -0,0 +1,15 @@
+package com.learning.spring.orders.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Client {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+}
---
+++ orders/src/test/java/com/learning/spring/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
