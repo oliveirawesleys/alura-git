f3db22a33f4c108a8926b649452d6e0baffb96cd
Wesley <oliveira.wesleys@gmail.com>
2021-04-07 19:53:30 -0300

    Aula 11 para certificação

--- certification-training/src/labs/pm/app/Shop.java
+++ certification-training/src/labs/pm/app/Shop.java
@@ -33,8 +33,10 @@ public class Shop {
         Comparator<Product> ratingSorter = (p1, p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
 
         Comparator<Product> priceSorter = (p1, p2) -> p2.getPrice().compareTo(p1.getPrice());
-        pm.printProducts(ratingSorter.thenComparing(priceSorter));
-        pm.printProducts(ratingSorter.thenComparing(priceSorter).reversed());
+        pm.printProducts(p -> p.getPrice().floatValue() < 2, ratingSorter.thenComparing(priceSorter));
+
+        pm.getDiscounts().forEach((rating, discount) -> System.out.println(rating + "\t" + discount));
+        //pm.printProducts(ratingSorter.thenComparing(priceSorter).reversed());
 //        pm.printProducts(priceSorter);
 //        pm.printProducts(ratingSorter);
 
