d043f2c6876540b466b27eb6b3a47db8f828b552
Wesley <oliveira.wesleys@gmail.com>
2021-05-05 18:05:57 -0300

    Implementando jogo da velha

---
+++ game-tictactoe/src/main/java/com/tictactoe/gametictactoe/GameTictactoeApplication.java
@@ -0,0 +1,13 @@
+package com.tictactoe.gametictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTictactoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTictactoeApplication.class, args);
+	}
+
+}
