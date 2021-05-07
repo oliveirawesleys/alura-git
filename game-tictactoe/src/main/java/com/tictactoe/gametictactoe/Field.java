ffadc3682795d784a776ebbad4bff003b7cd966f
Wesley <oliveira.wesleys@gmail.com>
2021-05-07 19:13:38 -0300

    Acrescentando funcionalidades no jogo

--- game-tictactoe/src/main/java/com/tictactoe/gametictactoe/Field.java
+++ game-tictactoe/src/main/java/com/tictactoe/gametictactoe/Field.java
@@ -2,11 +2,24 @@ package com.tictactoe.gametictactoe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Field extends JButton {
+    final int PLAYER_1 = 1;
+    final int PLAYER_2 = 2;
+
+    int playerActual = PLAYER_1;
 
     public Field() {
         int who = 0;
         setBackground(Color.WHITE);
+        addActionListener(e -> {
+            if (who == 0) {
+                if (playerActual == PLAYER_1) {
+                } else {
+                }
+            }
+        });
     }
 }
--- game-tictactoe/src/main/java/com/tictactoe/gametictactoe/TicTacToe.java
+++ game-tictactoe/src/main/java/com/tictactoe/gametictactoe/TicTacToe.java
@@ -5,13 +5,19 @@ import java.awt.*;
 
 public class TicTacToe extends JFrame {
 
+    ImageIcon circle = new ImageIcon("circle.png");
+    ImageIcon x = new ImageIcon("x.png");
+
     JPanel pScreen = new JPanel(new GridLayout(3, 3, 5, 5));
-    JLabel lInformation = new JLabel();
 
     Field[] fields = new Field[9];
 
     final int PLAYER_1 = 1;
     final int PLAYER_2 = 2;
+
+    int playerActual = PLAYER_1;
+
+    JLabel lInformation = new JLabel("Player " + PLAYER_1);
 
     public TicTacToe() {
         configureTicTacToe();
@@ -21,6 +27,10 @@ public class TicTacToe extends JFrame {
     public void configureScreen() {
         add(BorderLayout.CENTER, pScreen);
         add(BorderLayout.NORTH, lInformation);
+        pScreen.setBackground(Color.BLACK);
+        lInformation.setFont(new Font("Arial", Font.BOLD, 35));
+        lInformation.setForeground(new Color(50, 200, 50));
+        lInformation.setHorizontalAlignment(SwingConstants.CENTER);
 
         for (int i = 0; i < 9; i++) {
             Field field = new Field();
@@ -30,6 +40,24 @@ public class TicTacToe extends JFrame {
         }
     }
 
+    public void changePlayer() {
+        if (playerActual == 1) {
+            playerActual = 2;
+            lInformation.setText("Player 2");
+            lInformation.setForeground(Color.RED);
+        } else {
+            playerActual = 1;
+            lInformation.setText("Player 2");
+            lInformation.setForeground(Color.GREEN);
+        }
+    }
+
+    public boolean hasVictory(int player) {
+        if (fields[0].who == player && fields[1].who == player && fields[2].who == player ) {
+            return true;
+        }
+        return false;
+    }
 
     private void configureTicTacToe() {
         setTitle("Game");
@@ -39,5 +67,24 @@ public class TicTacToe extends JFrame {
         setVisible(true);
     }
 
+    public class Field extends JButton {
+        int who = 0;
+
+        public Field() {
+            setBackground(Color.WHITE);
+            addActionListener(e -> {
+                if (who == 0) {
+                    if (playerActual == PLAYER_1) {
+                        setIcon(circle);
+                        who = PLAYER_1;
+                    } else {
+                        setIcon(x);
+                        who = PLAYER_2;
+                    }
+                    changePlayer();
+                }
+            });
+        }
+    }
 
 }
