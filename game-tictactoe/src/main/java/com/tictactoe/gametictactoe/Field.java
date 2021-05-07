d32c680296aaaed1105c25678f93f82b89a1b722
Wesley <oliveira.wesleys@gmail.com>
2021-05-07 18:25:52 -0300

    Acrescentando funcionalidades no jogo

---
+++ game-tictactoe/src/main/java/com/tictactoe/gametictactoe/Field.java
@@ -0,0 +1,12 @@
+package com.tictactoe.gametictactoe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JButton {
+
+    public Field() {
+        int who = 0;
+        setBackground(Color.WHITE);
+    }
+}
--- game-tictactoe/src/main/java/com/tictactoe/gametictactoe/TicTacToe.java
+++ game-tictactoe/src/main/java/com/tictactoe/gametictactoe/TicTacToe.java
@@ -5,9 +5,31 @@ import java.awt.*;
 
 public class TicTacToe extends JFrame {
 
+    JPanel pScreen = new JPanel(new GridLayout(3, 3, 5, 5));
+    JLabel lInformation = new JLabel();
+
+    Field[] fields = new Field[9];
+
+    final int PLAYER_1 = 1;
+    final int PLAYER_2 = 2;
+
     public TicTacToe() {
         configureTicTacToe();
+        configureScreen();
     }
+
+    public void configureScreen() {
+        add(BorderLayout.CENTER, pScreen);
+        add(BorderLayout.NORTH, lInformation);
+
+        for (int i = 0; i < 9; i++) {
+            Field field = new Field();
+
+            fields[i] = field;
+            pScreen.add(field);
+        }
+    }
+
 
     private void configureTicTacToe() {
         setTitle("Game");
@@ -16,4 +38,6 @@ public class TicTacToe extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
+
 }
