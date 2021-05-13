7c5dd87d48d563eaac3f6774e69f706548b3d102
Wesley <oliveira.wesleys@gmail.com>
2021-05-12 23:06:47 -0300

    Implementação de Getmapping

--- training-project/src/main/java/com/training/trainingproject/api/controller/UserController.java
+++ training-project/src/main/java/com/training/trainingproject/api/controller/UserController.java
@@ -1,22 +1,45 @@
 package com.training.trainingproject.api.controller;
 
 import com.training.trainingproject.domain.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.training.trainingproject.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @PersistenceContext
+/*    @PersistenceContext
     private EntityManager manager;
 
     @GetMapping("/users")
     public List<User> list() {
         return manager.createQuery("from User", User.class).getResultList();
+    }*/
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> list() {
+        return userRepository.findAll();
     }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> search(@PathVariable Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- training-project/src/main/java/com/training/trainingproject/domain/repository/UserRepository.java
+++ training-project/src/main/java/com/training/trainingproject/domain/repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.training.trainingproject.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByName(String name);
+    List<User> findByNameContaining(String name);
+
 }
