460af9a8b3cd002a4ab5631b3fe444f482b1b654
Wesley <oliveira.wesleys@gmail.com>
2021-05-18 23:43:20 -0300

    Acrescentando arquivo de messages.properties

--- training-project/src/main/java/com/training/trainingproject/api/exceptionhandler/ApiExceptionHandler.java
+++ training-project/src/main/java/com/training/trainingproject/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.training.trainingproject.api.exceptionhandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -16,6 +20,9 @@ import java.util.ArrayList;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
                                                                   HttpStatus status, WebRequest request) {
@@ -23,11 +30,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         for (ObjectError error : ex.getBindingResult().getAllErrors()) {
             String name = ((FieldError) error).getField();
-            String message = error.getDefaultMessage();
+            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 
             fields.add(new Problem.Field(name, message));
         }
-        
+
         var problem = new Problem();
         problem.setStatus(status.value());
         problem.setTitle("Um ou mais campos estão inválidos!");
