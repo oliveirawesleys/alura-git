7882427598622823a2df59c4666483ea13f2d9ea
Wesley <oliveira.wesleys@gmail.com>
2021-05-24 20:08:22 -0300

    Implementando model mapper

--- training-project/src/main/java/com/training/trainingproject/api/controller/OrderWorkController.java
+++ training-project/src/main/java/com/training/trainingproject/api/controller/OrderWorkController.java
@@ -1,8 +1,10 @@
 package com.training.trainingproject.api.controller;
 
+import com.training.trainingproject.api.model.OrderWorkModel;
 import com.training.trainingproject.domain.model.OrderWork;
 import com.training.trainingproject.domain.repository.OrderWorkRepository;
 import com.training.trainingproject.domain.service.ManagementOrderWorkService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,35 +13,50 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
-        @RequestMapping("/order-work")
-        public class OrderWorkController {
+@RequestMapping("/order-work")
+public class OrderWorkController {
 
-            @Autowired
-            private ManagementOrderWorkService managementOrderWorkService;
+    @Autowired
+    private ManagementOrderWorkService managementOrderWorkService;
 
-            @Autowired
-            private OrderWorkRepository orderWorkRepository;
+    @Autowired
+    private OrderWorkRepository orderWorkRepository;
 
-            @PostMapping
-            @ResponseStatus(HttpStatus.CREATED)
-            public OrderWork create(@Valid @RequestBody OrderWork orderWork) {
-                return managementOrderWorkService.create(orderWork);
-            }
+    @Autowired
+    private ModelMapper modelMapper;
 
-            @GetMapping
-            public List<OrderWork> listAll() {
-                return orderWorkRepository.findAll();
-            }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderWorkModel create(@Valid @RequestBody OrderWork orderWork) {
+        return toModel(managementOrderWorkService.create(orderWork));
+    }
 
-            @GetMapping("/{orderWorkId}")
-            public ResponseEntity<OrderWork> search(@PathVariable Long orderWorkId) {
-                Optional<OrderWork> orderWord = orderWorkRepository.findById(orderWorkId);
+    @GetMapping
+    public List<OrderWorkModel> listAll() {
+        return toCollectionModel(orderWorkRepository.findAll());
+    }
 
-                if (orderWord.isPresent()) {
-                    return ResponseEntity.ok(orderWord.get());
-                }
-                return ResponseEntity.notFound().build();
-            }
+    @GetMapping("/{orderWorkId}")
+    public ResponseEntity<OrderWorkModel> search(@PathVariable Long orderWorkId) {
+        Optional<OrderWork> orderWork = orderWorkRepository.findById(orderWorkId);
+
+        if (orderWork.isPresent()) {
+            OrderWorkModel orderWorkModel = toModel(orderWork.get());
+            return ResponseEntity.ok(orderWorkModel);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    private OrderWorkModel toModel(OrderWork orderWork) {
+        return modelMapper.map(orderWork, OrderWorkModel.class);
+    }
+
+    private List<OrderWorkModel> toCollectionModel(List<OrderWork> ordersWork) {
+        return ordersWork.stream().map(orderWork -> toModel(orderWork))
+                .collect(Collectors.toList());
+    }
+
 }
