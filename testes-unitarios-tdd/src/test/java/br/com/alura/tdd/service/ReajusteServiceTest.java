7bd682a2aa0320c66fd717f577667eccf0074a9c
Wesley <oliveira.wesleys@gmail.com>
2021-05-23 22:32:00 -0300

    Implementando teste unitarios

--- testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.math.BigDecimal;
