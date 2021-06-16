7f6296da22b3892d444066fe4c10b358c59d3be1
Wesley <oliveira.wesleys@gmail.com>
2021-06-15 21:58:27 -0300

    Implementando JSTL e tratamento de data

--- gerenciador/WebContent/novaEmpresaCriada.jsp
+++ gerenciador/WebContent/novaEmpresaCriada.jsp
@@ -1,12 +1,15 @@
-<%
-	String nomeEmpresa = (String) request.getAttribute("empresa");
-	System.out.println(nomeEmpresa);
-%>
-
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <html>
-<head>
-<body>
-	Empresa <%= nomeEmpresa %> cadastrada com sucesso!
-</body>
-</html>
+	<body>
+	
+		<c:if test="${not empty empresa }">
+			Empresa ${empresa} cadastrada com sucesso!
+		</c:if>
+		
+		<c:if test="${empty empresa }">
+			Nenhuma empresa cadastrada!
+		</c:if>	
+		
+	</body>
+</html>	
--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -2,13 +2,16 @@ package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/novaEmpresa")
@@ -20,8 +23,20 @@ public class NovaEmpresaServlet extends HttpServlet {
 		System.out.println("Cadastrando nova empresa.");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date dataAbertura = null;
+
+		try {
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
