e4e320949a7a39d24930e4074bd47e554e3fb206
Wesley <oliveira.wesleys@gmail.com>
2021-06-29 21:59:58 -0300

    Finalizando curso e realizando deploy no Jetty

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -16,6 +16,12 @@ import javax.servlet.http.HttpSession;
 //@WebFilter("/entrada")
 public class AutorizacaoFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
 		System.out.println("AutorizacaoFilter");
