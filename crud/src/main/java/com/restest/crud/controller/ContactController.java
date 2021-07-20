04065eb3c68a3abbde2e869967b738af8e3f15f7
Wesley <oliveira.wesleys@gmail.com>
2021-07-20 19:01:02 -0300

    Metodos para CRUD

--- crud/src/main/java/com/restest/crud/controller/ContactController.java
+++ crud/src/main/java/com/restest/crud/controller/ContactController.java
@@ -1,8 +1,12 @@
 package com.restest.crud.controller;
 
+import com.restest.crud.model.Contact;
 import com.restest.crud.repository.ContactRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contacts")
@@ -14,5 +18,15 @@ public class ContactController {
         this.repository = repository;
     }
 
-    
+    @GetMapping
+    public List findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findByOne(@PathVariable Long id){
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
