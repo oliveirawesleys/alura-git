66676fb35c36ae197c75c6b36e7647f81ef1090c
Wesley <oliveira.wesleys@gmail.com>
2021-07-27 19:52:08 -0300

    Criação de metodos para CRUD

--- crud/src/main/java/com/restest/crud/controller/ContactController.java
+++ crud/src/main/java/com/restest/crud/controller/ContactController.java
@@ -1,11 +1,9 @@
 package com.restest.crud.controller;
 
+import com.restest.crud.model.Contact;
 import com.restest.crud.repository.ContactRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +23,27 @@ public class ContactController {
     }
 
     @GetMapping(path = {"/{id}"})
-    public ResponseEntity findByOne(@PathVariable Long id){
+    public ResponseEntity findById(@PathVariable Long id){
         return repository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Contact create(@RequestBody Contact contact) {
+        return repository.save(contact);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity update(@PathVariable("id") Long id,
+                                    @RequestBody Contact contact) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
